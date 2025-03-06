40aa2175781ac23587dee1a6fc0c47780098c7d3
Musser_James <mussj247@hsd.k12.or.us>
2025-03-06 11:08:02 -0800

    fixed Camra

--- Unit 1/Lesson 1/Assets/Scrpit/PlayerController.cs
+++ Unit 1/Lesson 1/Assets/Scrpit/PlayerController.cs
@@ -10,8 +10,8 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     
-    private float speed = 200f;
-    private float turnSpeed = 300f;
+    private float speed = 50f;
+    private float turnSpeed = 100f;
     private float horizontalInput;
     private float forwardInput;
     void Update()
